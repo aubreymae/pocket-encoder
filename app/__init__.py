9a931a3b693d570eea3f0b0b893891100a85d6e6
Aubrey Garcia <aubreymaerg@gmail.com>
2026-08-17 20:01:17 -0400

    Changed file extension when storing db

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev", 
-        DATABASE=os.path.join(app.instance_path, "upload-db.sqlite"),
+        DATABASE=os.path.join(app.instance_path, "upload-db.db"),
         UPLOAD_FOLDER=os.path.join(app.root_path, "uploads"),
         PROCESSED_FOLDER=os.path.join(app.root_path, "processed")
     )
