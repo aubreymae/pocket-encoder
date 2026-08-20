17d92c6bbf647ba31a0bc3818233bbd4883c0828
Aubrey Garcia <aubreymaerg@gmail.com>
2026-08-20 17:50:18 -0400

    Updated SQL schema

--- app/schema.sql
+++ app/schema.sql
@@ -5,3 +5,9 @@ create table videos (
     filename text unique not null,
     filesize integer
 );
+
+create table jobs (
+    job_id integer primary key autoincrement,
+    job_status text unique not null,
+    timestamp datetime default current_timestamp
+);
