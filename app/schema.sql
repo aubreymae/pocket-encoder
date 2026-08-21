1ce2113b07e5a47c1249783c3d45b6d1887aba50
Aubrey Garcia <aubreymaerg@gmail.com>
2026-08-21 19:46:12 -0400

    Updated schema

--- app/schema.sql
+++ app/schema.sql
@@ -2,12 +2,13 @@ drop table if exists videos;
 
 create table videos (
     id integer primary key autoincrement,
-    filename text unique not null,
-    filesize integer
-);
-
-create table jobs (
-    job_id integer primary key autoincrement,
-    job_status text unique not null,
-    timestamp datetime default current_timestamp
+    rq_job_id TEXT,
+    filename text not null,
+    input_path text not null,
+    output_path text,
+    target_size_mb real,
+    status text not null check(status in ('Queued', 'Processing', 'Completed', 'Failed')),
+    error_message text,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp
 );
